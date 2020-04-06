b94fabbc4f601c23b9f9e1e9a9c05bcfe7f37995
IT PARLOUR <itparlour360@gmail.com>
2020-04-06 13:43:58 +0600

    make installable

--- manifest.php
+++ manifest.php
@@ -1,18 +1,33 @@
-
 {
-  "version":"1.0.0",
-  "name": "<?php echo 'App name'; ?>",
-  "short_name": "PWA",
+  "name": "Wordpress theme build with react.js",
+  "short_name": "Wp React",
+  "icons": [{
+    "src": "<?php echo dirname($_SERVER['PHP_SELF']).'/assets/images/icons/icon-128x128.png';?>",
+      "sizes": "128x128",
+      "type": "image/png"
+    }, {
+      "src": "<?php echo dirname($_SERVER['PHP_SELF']).'/assets/images/icons/icon-144x144.png';?>",
+      "sizes": "144x144",
+      "type": "image/png"
+    }, {
+      "src": "<?php echo dirname($_SERVER['PHP_SELF']).'/assets/images/icons/icon-152x152.png';?>",
+      "sizes": "152x152",
+      "type": "image/png"
+    }, {
+      "src": "<?php echo dirname($_SERVER['PHP_SELF']).'/assets/images/icons/icon-192x192.png';?>",
+      "sizes": "192x192",
+      "type": "image/png"
+    }, {
+      "src": "<?php echo dirname($_SERVER['PHP_SELF']).'/assets/images/icons/icon-256x256.png';?>",
+      "sizes": "256x256",
+      "type": "image/png"
+    }, {
+      "src": "<?php echo dirname($_SERVER['PHP_SELF']).'/assets/images/icons/icon-512x512.png';?>",
+      "sizes": "512x512",
+      "type": "image/png"
+    }],
   "start_url": ".",
   "display": "standalone",
-  "background_color": "#000",
-  "description": "A simply PWA build by Sankar Bala.",
-  "theme_color":"#333",
-  "orientation":"landscape",
-  "icons": [{
-    "src": "favicon.png",
-    "sizes": "484x484",
-    "type": "image/png"
-  }]
+  "background_color": "#ff0000",
+  "theme_color": "#2F3BA2"
 }
-
